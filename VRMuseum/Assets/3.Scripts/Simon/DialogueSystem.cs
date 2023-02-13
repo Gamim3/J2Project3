d84e9d0b31c4296d047e968307d4cf491b7e6acc
SimonWameling <simon.wameling@gmail.com>
2023-02-13 15:45:31 +0100

    Dialog System update
    still WIP

--- VRMuseum/Assets/3.Scripts/Simon/DialogueSystem.cs
+++ VRMuseum/Assets/3.Scripts/Simon/DialogueSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour
 {
@@ -15,4 +16,47 @@ public class DialogueSystem : MonoBehaviour
     {
         
     }
+
+    public class Dialogue
+    {
+    public string speaker;
+    public string line;
+    }
+
+    public class DialogueManager : MonoBehaviour
+{
+    public List<Dialogue> dialogues;
+    private int currentDialogue;
+
+    public Text speakerText;
+    public Text dialogueText;
+
+    public GameObject dialogueBox;
+
+    public void ShowDialogue()
+    {
+        Dialogue current = dialogues[currentDialogue];
+        speakerText.text = current.speaker;
+        dialogueText.text = current.line;
+        dialogueBox.SetActive(true);
+    }
+
+    public void NextDialogue()
+    {
+        currentDialogue++;
+        if (currentDialogue >= dialogues.Count)
+        {
+            HideDialogue();
+            return;
+        }
+        ShowDialogue();
+    }
+
+    public void HideDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+}
+
+
 }
