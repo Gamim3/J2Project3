e8ce75a533a6932603a57c2f138bab53e197e0b5
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-02-20 11:25:31 +0100

    UX + prefab Xr origin

---
+++ VRMuseum/Assets/3.Scripts/Maykel/UX/CharUX.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CharUX : MonoBehaviour
+{
+    [SerializeField] private ContinuousMoveProviderBase movement;
+    [SerializeField] private TeleportationProvider teleport;
+    [SerializeField] private 
+}
