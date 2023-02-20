9b9ac64e313243d411764f7e7669fce4571a4258
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-02-20 11:30:15 +0100

    uxchar make

--- VRMuseum/Assets/3.Scripts/Maykel/UX/CharUX.cs
+++ VRMuseum/Assets/3.Scripts/Maykel/UX/CharUX.cs
@@ -7,5 +7,12 @@ public class CharUX : MonoBehaviour
 {
     [SerializeField] private ContinuousMoveProviderBase movement;
     [SerializeField] private TeleportationProvider teleport;
-    [SerializeField] private 
+    [SerializeField] private ContinuousTurnProviderBase smoothTurn;
+    [SerializeField] private SnapTurnProviderBase snapTurn;
+
+    private void Start()
+    {
+        teleport.enabled = true;
+        snapTurn.enabled = true;
+    }
 }
