b6ba1c06d7da108bda52691f7acfad2b3bd4675b
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-02-20 12:43:56 +0100

    fix

--- VRMuseum/Assets/3.Scripts/Maykel/UX/CharUX.cs
+++ VRMuseum/Assets/3.Scripts/Maykel/UX/CharUX.cs
@@ -10,9 +10,30 @@ public class CharUX : MonoBehaviour
     [SerializeField] private ContinuousTurnProviderBase smoothTurn;
     [SerializeField] private SnapTurnProviderBase snapTurn;
 
+
     private void Start()
     {
         teleport.enabled = true;
         snapTurn.enabled = true;
+    }
+    public void MovementBTN()
+    {
+        movement.enabled = true;
+        teleport.enabled = false;
+    }
+    public void TeleportBTN()
+    {
+        teleport.enabled = true;
+        movement.enabled = true;
+    }
+    public void SmoothTurnBTN()
+    {
+        smoothTurn.enabled = true;
+        snapTurn.enabled = false;
+    }
+    public void SnapTurnBTN()
+    {
+        snapTurn.enabled = true;
+        smoothTurn.enabled = false;
     }
 }
