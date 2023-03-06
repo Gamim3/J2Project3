2be9083a2361fc11b1c7273fbe17453c375a6ad5
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-03-06 11:11:38 +0100

    AR brill

---
+++ VRMuseum/Assets/3.Scripts/Maykel/AR/ARbrill.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARbrill : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer[] arblocks;
+
+    public void RenderBlocks()
+    {
+        for (int i = 0; i < arblocks.Length; i++)
+        {
+            arblocks[i].enabled = true;
+        }
+    }
+}
